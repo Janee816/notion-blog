ba5ce175cd67955bf39a833868a3ba5681afcdce
Janee816 <yanjingyi1996@163.com>
2024-11-25 11:36:14 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'neizai',
   domain: 'naizai.link',
-  author: 'Janee',
+  author: 'Janee宜笑',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
